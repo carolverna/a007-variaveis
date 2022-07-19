3196c6616916a9d72e51f48485c7a84a8f89524d
Carol Verna <anacarolinaverna@hotmail.com>
2022-07-19 17:37:59 -0300

    Exercício Concluido

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+
+const perguntaA = "Você conseguiu fazer o exercício 1?"
+let respostaA = prompt(perguntaA)
+
+console.log(perguntaA, respostaA)
+
+const perguntaB = "Você assistiu a aula de ontem?"
+let respostaB = prompt(perguntaB)
+
+console.log(perguntaB, respostaB)
+
+const perguntaC = "Você já sabia JS?"
+let respostaC = prompt(perguntaC)
+
+console.log(perguntaC, respostaC)
